357510635afc07f77889131406e2b507083aaa80
Nehal Patel <nehal@alum.mit.edu>
2015-01-26 12:36:54 -0500

    ++ index for crumpled paper

---
+++ scripts/generate-crumpled-index.rb
@@ -0,0 +1,27 @@
+#! /usr/bin/env ruby
+raise 'Please run from base lilKanren dir' unless Dir.exists? 'crumpled-paper'
+
+require 'pathname'
+
+def makeindex(path, file, level = 0)
+  puts path
+  address = "http://nbviewer.ipython.org/github/lilinjn/lilKanren/blob/master/"
+  path.children.collect do |child|
+    if child.file? and child.extname == ".ipynb"
+      file.write((' ' * level) + '* ' + child.basename.to_s + ": #{address}#{child.to_s}" + "\n")
+    elsif child.directory? and !child.basename.to_s.start_with?('.')
+      file.write((' ' * level) + '* ' + child.basename.to_s + "\n")
+      makeindex(child, file, level+1)
+    end
+  end
+end
+
+
+File.open('crumpled-paper/Index.MD', 'w') do |file|
+  header = <<HEADER
+Crumpled Paper
+===============
+HEADER
+  file.write(header)
+  makeindex(Pathname.new('crumpled-paper'), file, 0)
+end
